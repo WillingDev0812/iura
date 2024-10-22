818066c6b15d16d5d789c10e81980a34f80ea1e6
WillingDev0812 <amdmin0316@gmail.com>
2024-10-22 00:05:15 -0300

    update

--- src/utils/TechCard.js
+++ src/utils/TechCard.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const TechCard = (props) => {
   return (
     <>
       <span title={props.name}>
-      <span className="medium iconify mySpan" data-icon={props.logo}></span>
+        <span className="medium iconify mySpan" data-icon={props.logo}></span>
       </span>
     </>
   );
